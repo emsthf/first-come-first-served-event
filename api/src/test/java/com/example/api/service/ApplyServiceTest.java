9657efbf6799a407140e778eb95abfc57525e5bd
emsthf <deunsol916@gmail.com>
2023-10-06 16:20:37 +0900

    test: 레이스 컨디션으로 인한 선착순 쿠폰 발행 개수 초과 테스트

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -27,5 +31,31 @@ class ApplyServiceTest {
         // then
         long count = couponRepository.count();
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    void 여러_명_응모() throws Exception {
+        // given
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);  // 병렬 작업을 간단하게 할 수 있게 해주는 자바 유틸리티 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);  // CountDownLatch: 스레드가 실행되기 전에 특정 작업이 끝나기를 기다리는 기능을 제공하는 클래스
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        // then
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 }
